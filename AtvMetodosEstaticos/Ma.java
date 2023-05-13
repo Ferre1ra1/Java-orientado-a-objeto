5d7f5dfccde5cde282b52bb8fba3f99332b73c38
G_G <100845421+Ferre1ra1@users.noreply.github.com>
2023-05-13 16:09:02 -0300

    Revisões
    Muito melhor, que se foda a instanciação !!!!

---
+++ AtvMetodosEstaticos/Ma.java
@@ -0,0 +1,9 @@
+package AtvMetodosEstaticos;
+
+public class Ma {
+    public static void main(String[] args) {
+        System.out.println("Valor de pi "+Matematica.pi);
+        System.out.println("O fatorial de "+Matematica.valor + " é "+Matematica.fatorial());
+
+    }
+}
---
+++ AtvMetodosEstaticos/Matematica.java
@@ -0,0 +1,20 @@
+package AtvMetodosEstaticos;
+
+public class Matematica {
+    static double pi = 3.1416;
+    static int valor = 5;
+    public static int fatorial()
+    {
+        int valor = 10;
+        int fat = 1;
+        for(int i = 1; i <= valor; i++)
+        {
+          fat *= i;
+        }
+        return fat;
+    }
+
+    
+    
+    
+}
